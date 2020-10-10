c1a33ba7a4199851e5266859ae46dcbdefdff61e
Serina Grill <gril0044@umn.edu>
2020-10-10 16:59:20 -0400

    edited version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'SerinasDecisionTree',         
   packages = ['SerinasDecisionTree'],   
-  version = '0.1',      
+  version = '0.2',      
   license='MIT',       
   description = 'A Python package for decision-tree based classification of data',   
   author = 'Serina Grill',                  
